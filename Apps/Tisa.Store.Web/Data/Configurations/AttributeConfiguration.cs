ff6b4543e37a1fd95c94512fe729b0b72cd06f07
ahadvirus <ahadvirus@yahoo.com>
2023-04-23 10:32:00 +0430

    Config new entities for EntityFramework

---
+++ Apps/Tisa.Store.Web/Data/Configurations/AttributeConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tisa.Store.Web.Models.Entities;
+
+namespace Tisa.Store.Web.Data.Configurations;
+
+public class AttributeConfiguration : IEntityTypeConfiguration<Attribute>
+{
+    public void Configure(EntityTypeBuilder<Attribute> builder)
+    {
+        builder.HasKey(attribute => attribute.Id);
+
+        builder.Property(attribute => attribute.Name)
+            .IsRequired();
+
+        builder.Property(attribute => attribute.Title)
+            .IsRequired();
+    }
+}
---
+++ Apps/Tisa.Store.Web/Data/Configurations/TypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Tisa.Store.Web.Models.Entities;
+
+namespace Tisa.Store.Web.Data.Configurations;
+
+public class TypeConfiguration : IEntityTypeConfiguration<Type>
+{
+    public void Configure(EntityTypeBuilder<Type> builder)
+    {
+        builder.HasKey(type => type.Id);
+
+        builder.Property(type => type.Kind)
+            .IsRequired();
+
+        builder.HasIndex(type => type.Kind)
+            .IsUnique();
+
+        builder.HasMany(type => type.Attributes)
+            .WithOne(attribute => attribute.Type)
+            .HasForeignKey(attribute => attribute.TypeId);
+    }
+}
