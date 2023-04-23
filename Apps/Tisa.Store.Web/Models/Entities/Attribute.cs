f9d25f40d425506b093cb8f24c30715a61d7bf87
ahadvirus <ahadvirus@yahoo.com>
2023-04-23 10:30:51 +0430

    Create two entities for system

---
+++ Apps/Tisa.Store.Web/Models/Entities/Attribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Tisa.Store.Web.Models.Entities;
+
+public class Attribute
+{
+    public Attribute()
+    {
+        Name = string.Empty;
+        Title = string.Empty;
+    }
+    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Title { get; set; }
+    public int TypeId { get; set; }
+    public virtual Type Type { get; set; }
+}
---
+++ Apps/Tisa.Store.Web/Models/Entities/Type.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Tisa.Store.Web.Models.Entities;
+
+public class Type
+{
+    public Type()
+    {
+        Kind = string.Empty;
+    }
+    
+    public int Id { get; set; }
+    public string Kind { get; set; }
+    public virtual ICollection<Attribute> Attributes { get; set; }
+}
