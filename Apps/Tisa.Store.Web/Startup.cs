fae101902562866f917317cb28bc161ef36712f5
ahadvirus <ahadvirus@yahoo.com>
2023-04-23 10:34:18 +0430

    Some changes by editor

--- Apps/Tisa.Store.Web/Startup.cs
+++ Apps/Tisa.Store.Web/Startup.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Tisa.Store.Web.Data.Contexts;
 
 namespace Tisa.Store.Web;
 
@@ -8,8 +15,33 @@ public class Startup
 {
     public static void ConfigurationService(IServiceCollection services)
     {
-        services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        string name = nameof(ApplicationContext).Replace(
+            nameof(HttpContext).Replace(
+                nameof(WebRequestMethods.Http),
+                string.Empty
+            ),
+            string.Empty
+        );
+
+        SqliteConnectionStringBuilder connectionStringBuilder = new SqliteConnectionStringBuilder()
+        {
+            DataSource = System.IO.Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                string.Format(
+                    "{0}.db",
+                    name
+                )
+            )
+        };
+
+        services.AddDbContext<ApplicationContext>(
+            options => options.UseInMemoryDatabase(name) /* .UseSqlite(connectionStringBuilder.ConnectionString) */);
+
+        services.Configure<JsonOptions>(options => { options.SerializerOptions.PropertyNameCaseInsensitive = false; });
+
+        services.AddControllers()
+            .AddJsonOptions(options => { options.JsonSerializerOptions.PropertyNameCaseInsensitive = false; });
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
     }
