bb9959c282db96f3becb3703216f795f95ac13e0
ahadvirus <ahadvirus@yahoo.com>
2023-04-27 01:11:08 +0430

    Develop a controller for type

---
+++ Apps/Tisa.Store.Web/Controllers/TypeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tisa.Store.Web.Data.Contexts;
+
+namespace Tisa.Store.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TypeController : ControllerBase
+{
+    private ApplicationContext Context { get; }
+
+    public TypeController(ApplicationContext context)
+    {
+        Context = context;
+    }
+
+    // GET
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        return Ok(await Context.Types.ToListAsync());
+    }
+}
