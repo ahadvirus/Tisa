de577d27f747e886e864b7fba076325a0760be34
ahadvirus <ahadvirus@yahoo.com>
2023-04-27 18:04:22 +0430

    Convention for group REPR Controllers in same group

---
+++ Apps/Tisa.Store.Web/Infrastructures/Routes/ControllerNameDocumentationConvention.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Tisa.Store.Web.Infrastructures.Routes;
+
+public class ControllerNameDocumentationConvention : IControllerModelConvention
+{
+    public void Apply(ControllerModel? controller)
+    {
+        if (controller == null)
+            return;
+
+        foreach (SelectorModel selector in controller.Selectors.Where(selector => selector.AttributeRouteModel != null))
+        {
+            if (selector.AttributeRouteModel != null)
+            {
+                if (!string.IsNullOrWhiteSpace(selector.AttributeRouteModel.Name))
+                    controller.ControllerName = selector.AttributeRouteModel.Name;
+            }
+        }
+    }
+}
