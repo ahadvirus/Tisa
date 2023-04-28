d937b4c2328dc80aae058d45d01305a79348551c
ahadvirus <ahadvirus@yahoo.com>
2023-04-28 17:59:54 +0430

    Install SQL and implement to EF Core

--- Apps/Tisa.Store.Web/Startup.cs
+++ Apps/Tisa.Store.Web/Startup.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Linq;
-using System.Net;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Json;
-using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
 using Tisa.Store.Web.Controllers;
 using Tisa.Store.Web.Data.Contexts;
 using Tisa.Store.Web.Infrastructures.Extensions;
-using Tisa.Store.Web.Infrastructures.Routes;
+using Tisa.Store.Web.Infrastructures.Routes.Constraints;
+using Tisa.Store.Web.Infrastructures.Routes.Conventions;
 
 namespace Tisa.Store.Web;
 
@@ -21,6 +20,7 @@ public class Startup
 {
     public static void ConfigurationService(IServiceCollection services)
     {
+        /*
         string name = nameof(ApplicationContext).Replace(
             nameof(HttpContext).Replace(
                 nameof(WebRequestMethods.Http),
@@ -28,20 +28,35 @@ public class Startup
             ),
             string.Empty
         );
+        */
 
-        SqliteConnectionStringBuilder connectionStringBuilder = new SqliteConnectionStringBuilder()
-        {
-            DataSource = System.IO.Path.Combine(
-                AppDomain.CurrentDomain.BaseDirectory,
-                string.Format(
-                    "{0}.db",
-                    name
+            /*
+            SqliteConnectionStringBuilder connectionStringBuilder = new SqliteConnectionStringBuilder()
+            {
+                DataSource = System.IO.Path.Combine(
+                    AppDomain.CurrentDomain.BaseDirectory,
+                    string.Format(
+                        "{0}.db",
+                        name
+                    )
                 )
-            )
+            };
+            */
+
+        MySqlConnectionStringBuilder connectionStringBuilder = new MySqlConnectionStringBuilder()
+        {
+            Server = "localhost",
+            Port = 3307,
+            Database = "TisaStore",
+            UserID = "root",
+            Password = "123@host.local"
         };
 
         services.AddDbContext<ApplicationContext>(
-            options => options.UseInMemoryDatabase(name) /* .UseSqlite(connectionStringBuilder.ConnectionString) */);
+            options => options.UseMySQL(connectionStringBuilder.ConnectionString)
+            /* .UseInMemoryDatabase(name) */
+            /* .UseSqlite(connectionStringBuilder.ConnectionString) */
+        );
 
         services.Configure<JsonOptions>(options => { options.SerializerOptions.PropertyNameCaseInsensitive = false; });
 
@@ -50,24 +65,44 @@ public class Startup
         services.AddControllers(options =>
             {
                 Type homeController = typeof(HomeController);
-                
+
                 string namespaceTokenValue =
                     !string.IsNullOrWhiteSpace(homeController.Namespace)
                         ? homeController.Namespace
                         : string.Empty;
 
-                options.Conventions.Insert(0, new NamespaceToken(namespaceTokenValue));
+                options.Conventions.Insert(0, new NamespaceTokenConvention(namespaceTokenValue));
+                options.Conventions.Insert(1, new ControllerTokenConvention());
                 options.Conventions.Add(new ControllerNameDocumentationConvention());
             })
             .AddJsonOptions(options => { options.JsonSerializerOptions.PropertyNameCaseInsensitive = false; });
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         services.AddEndpointsApiExplorer();
+
         services.AddSwaggerGen(options =>
         {
             options.CustomSchemaIds(type => type.GetCustomAttributes()
                 .Where(attribute => attribute.GetType() == typeof(DisplayNameAttribute))
                 .Select(attribute => ((DisplayNameAttribute)attribute).DisplayName)
                 .FirstOrDefault());
+        });
+
+        services.AddRouting(options =>
+        {
+            options.LowercaseUrls = true;
+            options.LowercaseQueryStrings = true;
+
+            options.ConstraintMap.Add(
+                nameof(Models.Entities.Entity)
+                    .ToLower(),
+                typeof(EntityConstraint)
+            );
+            
+            options.ConstraintMap.Add(
+                nameof(Models.Entities.AttributeEntity)
+                    .ToLower(),
+                typeof(AttributeEntityConstraint)
+            );
         });
     }
 
