49670062338247c9f9921f1b47e531d792951289
ahadvirus <ahadvirus@yahoo.com>
2023-05-02 00:07:45 +0430

    Create get method for represent all validators

--- Apps/Tisa.Store.Web/Controllers/Validator/IndexController.cs
+++ Apps/Tisa.Store.Web/Controllers/Validator/IndexController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tisa.Store.Web.Data.Contexts;
+using Tisa.Store.Web.Models.ViewModels.Validators;
 
 namespace Tisa.Store.Web.Controllers.Validator;
 
@@ -9,4 +17,21 @@ namespace Tisa.Store.Web.Controllers.Validator;
 )]
 public class IndexController : ControllerBase
 {
+    private ApplicationContext Context { get; }
+
+    private IMapper Mapper { get; }
+
+    public IndexController(ApplicationContext context, IMapper mapper)
+    {
+        Context = context;
+        Mapper = mapper;
+    }
+
+    public async Task<ActionResult<IEnumerable<IndexVM>>> Invoke(CancellationToken cancellationToken)
+    {
+        return Ok(
+            await Context.Validators.ProjectTo<IndexVM>(Mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken)
+        );
+    }
 }
