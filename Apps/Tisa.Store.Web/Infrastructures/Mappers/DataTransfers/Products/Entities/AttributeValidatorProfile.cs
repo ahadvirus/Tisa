d12492c19c0b715b47e539d2762d6f908c98b952
ahadvirus <ahadvirus@yahoo.com>
2023-05-04 18:12:02 +0430

    Some profiles to convert entities to their data transfers object

---
+++ Apps/Tisa.Store.Web/Infrastructures/Mappers/DataTransfers/Products/Entities/AttributeValidatorProfile.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using AutoMapper;
+
+namespace Tisa.Store.Web.Infrastructures.Mappers.DataTransfers.Products.Entities;
+
+public class AttributeValidatorProfile : Profile
+{
+    public AttributeValidatorProfile()
+    {
+        CreateMap<Models.Entities.AttributeEntity, Models.DataTransfers.Products.Entities.AttributeEntityDTO>()
+            .ForMember(des => des.Id,
+                opt => opt.MapFrom(
+                    src => src.Id
+                ))
+            .ForMember(des => des.Name,
+                opt => opt.MapFrom(
+                    src => src.Attribute.Name
+                ))
+            .ForMember(des => des.Type,
+                opt => opt.MapFrom(
+                    src => src.Attribute.Type.Name
+                ))
+            .ForMember(des => des.GetType,
+                opt => opt.Ignore())
+            .ForMember(des => des.Validators,
+                opt => opt.MapFrom(
+                    src => src.Validators
+                        .Where(validator =>
+                            validator.Validator.Types.Any(type => type.TypeId == src.Attribute.TypeId))
+                ));
+
+        CreateMap<Models.Entities.AttributeEntity, Infrastructures.Contracts.DataTransfers.IAttributeEntityDTO>()
+            .As<Models.DataTransfers.Products.Entities.AttributeEntityDTO>();
+
+        CreateMap<Models.DataTransfers.Products.Entities.AttributeEntityDTO,
+            Models.DataTransfers.Products.Entities.AttributeDTO>();
+
+        CreateMap<Infrastructures.Contracts.DataTransfers.IAttributeEntityDTO,
+                Infrastructures.Contracts.DataTransfers.IAttributeDTO>()
+            .As<Models.DataTransfers.Products.Entities.AttributeDTO>();
+    }
+}
---
+++ Apps/Tisa.Store.Web/Infrastructures/Mappers/DataTransfers/Products/Entities/ValidatorProfile.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using AutoMapper;
+
+namespace Tisa.Store.Web.Infrastructures.Mappers.DataTransfers.Products.Entities;
+
+public class ValidatorProfile : Profile
+{
+    public ValidatorProfile()
+    {
+        CreateMap<Models.Entities.AttributeEntityValidator, Models.DataTransfers.Products.Entities.AttributeValidatorDTO>()
+            .ForMember(des => des.Name,
+                opt => opt.MapFrom(
+                    src => src.Validator.Name
+                ))
+            .ForMember(des => des.Parameters,
+                opt => opt.MapFrom(
+                    src => src.Claims
+                        .Where(claim =>
+                            claim.Key == nameof(Contracts.Claims.AttributeEntityValidator.IParameter.Parameters))
+                        .Select(claim => claim.Value)
+                        .First()
+                ))
+            .ForMember(des => des.ParametersValidator,
+                opt => opt.Ignore())
+            .ForMember(des => des.Validator,
+                opt => opt.MapFrom(
+                    src => src.Validator.Claims
+                        .Where(claim =>
+                            claim.Key == nameof(Contracts.Claims.Validators.IParameter.ParametersValidator))
+                        .Select(claim => claim.Value)
+                        .First()
+                ))
+            .ForMember(des => des.ValidatorBuilder,
+                opt => opt.Ignore())
+            .ForMember(des => des.Builder,
+                opt => opt.MapFrom(
+                    src => src.Validator.Claims
+                        .Where(claim =>
+                            claim.Key == nameof(Contracts.Claims.Validators.IBuilder.ValidatorBuilder))
+                        .Select(claim => claim.Value)
+                        .First()
+                ));
+        
+        CreateMap<Models.Entities.AttributeEntityValidator, Infrastructures.Contracts.DataTransfers.IValidatorDTO>()
+            .As<Models.DataTransfers.Products.Entities.AttributeValidatorDTO>();
+
+    }
+}
