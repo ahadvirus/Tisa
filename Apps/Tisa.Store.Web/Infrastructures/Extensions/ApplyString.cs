5b16ec562b80c9efe1023fb698b02f83f29788b2
ahadvirus <ahadvirus@yahoo.com>
2023-05-09 14:20:06 +0430

    Save some changes

---
+++ Apps/Tisa.Store.Web/Infrastructures/Extensions/ApplyString.cs
@@ -0,0 +1,14 @@
+﻿namespace Tisa.Store.Web.Infrastructures.Extensions;
+
+public static class ApplyString
+{
+    public static string RemoveNumber(this string value)
+    {
+        return value;
+    }
+
+    public static string GetName(this System.Type systems)
+    {
+        return systems.GetName();
+    }
+}
