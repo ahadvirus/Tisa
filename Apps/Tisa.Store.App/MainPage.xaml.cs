54785595d0a8233af3c79a94206764e23ead3e69
ahadvirus <ahadvirus@yahoo.com>
2023-05-09 14:20:31 +0430

    Start to deply windows application base on MAUI

---
+++ Apps/Tisa.Store.App/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Tisa.Store.App;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Apps/Tisa.Store.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Tisa.Store.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
