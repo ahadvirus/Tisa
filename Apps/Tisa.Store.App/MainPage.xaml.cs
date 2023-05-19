6877b057f8029ed1778a3e93094784b018bc25af
ahadvirus <ahadvirus@yahoo.com>
2023-05-19 10:40:30 +0330

    Remove useless projects from solution

--- Apps/Tisa.Store.App/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tisa.Store.App;
-
-public partial class MainPage : ContentPage
-{
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-}
--- Apps/Tisa.Store.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Tisa.Store.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
