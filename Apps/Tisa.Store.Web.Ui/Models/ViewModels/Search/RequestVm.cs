4f07793e21bdcec90a488bd511ee590e4dd20f06
ahadvirus <ahadvirus@yahoo.com>
2023-05-20 15:14:52 +0330

    Create basic search mechanism

---
+++ Apps/Tisa.Store.Web.Ui/Models/ViewModels/Search/RequestVm.cs
@@ -0,0 +1,11 @@
+﻿namespace Tisa.Store.Web.Ui.Models.ViewModels.Search;
+
+public record RequestVm
+{
+    public RequestVm()
+    {
+        Query = string.Empty;
+    }
+
+    public string Query { get; init; }
+}
---
+++ Apps/Tisa.Store.Web.Ui/Models/ViewModels/Search/ResponseVm.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tisa.Store.Web.Ui.Models.ViewModels.Search;
+
+public record ResponseVm<T> : IEnumerable<T>
+{
+    public ResponseVm(IEnumerable<T> results)
+    {
+        Query = string.Empty;
+        Results = results;
+    }
+
+    [Display]
+    public string Query { get; init; }
+
+    private IEnumerable<T> Results { get; init; }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return Results.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
