b7822ac38557d193fb0e118627fcdc76286cd765
ahadvirus <ahadvirus@yahoo.com>
2023-05-20 15:35:15 +0330

    Create controller and view models and some views for attribute

--- Apps/Tisa.Store.Web.Ui/Controllers/AttributesController.cs
+++ Apps/Tisa.Store.Web.Ui/Controllers/AttributesController.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tisa.Store.Web.Ui.Data.Repositories.Contracts;
+using Tisa.Store.Web.Ui.Models.DataTransfers;
+using Tisa.Store.Web.Ui.Models.ViewModels.Attributes;
 
 namespace Tisa.Store.Web.Ui.Controllers;
 
 public class AttributesController : Controller
 {
+    private IAttributeRepository Repository { get; }
+
+    public AttributesController(IAttributeRepository repository)
+    {
+        Repository = repository;
+    }
+
+
     // GET
-    public IActionResult Index()
+    public async Task<IActionResult> Index([FromQuery] Models.ViewModels.Search.RequestVm request)
+    {
+        Func<Models.DataTransfers.AttributeDto, bool>? predicate = null;
+
+        if (!string.IsNullOrEmpty(request.Query))
+        {
+            predicate = dto => dto.Display.Contains(request.Query);
+        }
+
+        return View(model: new Models.ViewModels.Search.ResponseVm<Models.DataTransfers.AttributeDto>(
+            results: await Repository.GetAsync(predicate: predicate)
+            )
+        {
+            Query = request.Query
+        });
+    }
+
+    public IActionResult Edit([FromRoute] int? id)
+    {
+        
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Create()
     {
         return View();
+    }
+
+    [ValidateAntiForgeryToken]
+    [ActionName(name:nameof(Create))]
+    [HttpPost]
+    public async Task<IActionResult> CreateConfirmed(
+        [Bind(include: new string[] { nameof(CreateVm.Display), nameof(CreateVm.Description), nameof(CreateVm.Type) })]
+        CreateVm entry
+    )
+    {
+        IActionResult result = View(model: entry);
+
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                await Repository.AddAsync(entry: new AttributeDto()
+                {
+                    Display = entry.Display,
+                    Description = entry.Description,
+                    TypeId = entry.Type
+                });
+
+                result = RedirectToAction(nameof(Index));
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError(key: "SomethingHappened", errorMessage: e.Message);
+            }
+        }
+
+        return result;
     }
 }
---
+++ Apps/Tisa.Store.Web.Ui/Models/ViewModels/Attributes/CreateVm.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tisa.Store.Web.Ui.Infrastructures.Attributes;
+using Tisa.Store.Web.Ui.Infrastructures.Configurations;
+
+namespace Tisa.Store.Web.Ui.Models.ViewModels.Attributes;
+
+public record CreateVm
+{
+    public CreateVm()
+    {
+        Display = string.Empty;
+        Description = string.Empty;
+    }
+
+    [Display]
+    [Required(ErrorMessage = nameof(Display) + nameof(DataAnnotations.Required) + nameof(RequiredAttribute.ErrorMessage))]
+    [RegularExpression(pattern: "^[A-Za-z0-9]+$", ErrorMessage = nameof(Display) + nameof(DataAnnotations.RegularExpression) + nameof(RegularExpressionAttribute.ErrorMessage))]
+    [PascalName(ErrorMessage = nameof(Display) + nameof(DataAnnotations.PascalName) + nameof(PascalNameAttribute.ErrorMessage))]
+    public string Display { get; init; }
+
+    [Display]
+    [Required(ErrorMessage = nameof(Description) + nameof(DataAnnotations.Required) + nameof(RequiredAttribute.ErrorMessage))]
+    [RegularExpression(pattern: "^[A-Za-z0-9\\s\\'\\\"\\,\\(\\)\\[\\]\\\\_\\-]+$", ErrorMessage = nameof(Display) + nameof(DataAnnotations.RegularExpression) + nameof(RegularExpressionAttribute.ErrorMessage))]
+    public string Description { get; init; }
+    
+
+    public int Type { get; set; }
+}
