bc47b9366878c49618ec18f9741846d92154ff57
ahadvirus <ahadvirus@yahoo.com>
2023-05-20 15:33:38 +0330

    Create new validation attribute

---
+++ Apps/Tisa.Store.Web.Ui/Infrastructures/Attributes/PascalNameAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Tisa.Store.Web.Ui.Infrastructures.Attributes;
+
+public class PascalNameAttribute : ValidationAttribute
+{
+
+    protected string Pattern
+    {
+        get
+        {
+            return "^[A-Z]$";
+        }
+    }
+
+    public PascalNameAttribute() : base(errorMessage: "Please start {0} with capital character")
+    {
+    }
+
+    public override bool IsValid(object? value)
+    {
+        return value != null ?
+            (value is string ?
+                new RegularExpressionAttribute(pattern: Pattern).IsValid(((string)value).First()) :
+                false)
+            : false;
+    }
+}
--- Apps/Tisa.Store.Web.Ui/Infrastructures/Configurations/DataAnnotations.cs
+++ Apps/Tisa.Store.Web.Ui/Infrastructures/Configurations/DataAnnotations.cs
@@ -1,15 +1,51 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using Tisa.Store.Web.Ui.Infrastructures.Attributes;
 
 namespace Tisa.Store.Web.Ui.Infrastructures.Configurations;
 
 public class DataAnnotations
 {
+    /// <summary>
+    /// Required attribute without part of attribute
+    /// </summary>
     public string Required
     {
         get
         {
-            return nameof(RequiredAttribute).Replace(oldValue: nameof(Attribute), newValue: string.Empty);
+            return RemoveAttribute(nameof(RequiredAttribute));
         }
+    }
+
+    /// <summary>
+    /// RegularExpression attribute without part of attribute
+    /// </summary>
+    public string RegularExpression
+    {
+        get
+        {
+            return RemoveAttribute(nameof(RegularExpressionAttribute));
+        }
+    }
+
+    /// <summary>
+    /// PascalName attribute without part of attribute
+    /// </summary>
+    public string PascalName
+    {
+        get
+        {
+            return RemoveAttribute(nameof(PascalNameAttribute));
+        }
+    }
+
+    /// <summary>
+    /// Remove <c>Attribute</c> word from string
+    /// </summary>
+    /// <param name="entry"><see cref="string"/> The parameter want to customize</param>
+    /// <returns><see cref="string"/> Result after the remove</returns>
+    protected string RemoveAttribute(string entry)
+    {
+        return entry.Replace(oldValue: nameof(Attribute), newValue: string.Empty);
     }
 }
