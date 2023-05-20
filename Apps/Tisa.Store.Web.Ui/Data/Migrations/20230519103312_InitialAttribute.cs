c3b50091e5c63f3bf77a9b10307fff7e2d394b34
ahadvirus <ahadvirus@yahoo.com>
2023-05-20 15:33:03 +0330

    Add new entity with their migrations

---
+++ Apps/Tisa.Store.Web.Ui/Data/Migrations/20230519103312_InitialAttribute.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Tisa.Store.Web.Ui.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialAttribute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attributes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: false),
+                    AttributeId = table.Column<int>(type: "int", nullable: false),
+                    TypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attributes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attributes_Types_TypeId",
+                        column: x => x.TypeId,
+                        principalTable: "Types",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_AttributeId",
+                table: "Attributes",
+                column: "AttributeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attributes_TypeId",
+                table: "Attributes",
+                column: "TypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attributes");
+        }
+    }
+}
--- Apps/Tisa.Store.Web.Ui/Models/Entities/Attribute.cs
+++ Apps/Tisa.Store.Web.Ui/Models/Entities/Attribute.cs
@@ -7,25 +7,34 @@ public class Attribute : IEntity<int>
     public Attribute()
     {
         Name = string.Empty;
-        Type = new Type();
+        Description = string.Empty;
     }
 
     /// <summary>
     /// Primary key
     /// </summary>
     public int Id { get; set; }
+
     /// <summary>
     /// Translated name for attribute
     /// </summary>
     public string Name { get; set; }
+
+    /// <summary>
+    /// Translated description for attribute
+    /// </summary>
+    public string Description { get; set; }
+
     /// <summary>
     /// The attribute id in the api
     /// </summary>
     public int AttributeId { get; set; }
+
     /// <summary>
     /// The type id related to the attribute
     /// </summary>
     public int TypeId { get; set; }
+
     /// <summary>
     /// Type the related to the attribute
     /// </summary>
