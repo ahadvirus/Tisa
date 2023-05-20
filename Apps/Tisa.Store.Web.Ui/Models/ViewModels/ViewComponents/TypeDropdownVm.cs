88dc77ded1bbc348de004a8b64881d38574db28c
ahadvirus <ahadvirus@yahoo.com>
2023-05-20 15:11:56 +0330

    Create dropdown to select types

---
+++ Apps/Tisa.Store.Web.Ui/Models/ViewModels/ViewComponents/TypeDropdownVm.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Tisa.Store.Web.Ui.Models.ViewModels.ViewComponents;
+
+public record TypeDropdownVm : IEnumerable<KeyValuePair<int, string>>
+{
+    public TypeDropdownVm(IDictionary<int, string> options)
+    {
+        Options = options;
+    }
+
+    public int Selected { get; init; }
+
+    public string Input { get; init; }
+    
+    protected IDictionary<int, string> Options { get; init; }
+    
+    public IEnumerator<KeyValuePair<int, string>> GetEnumerator()
+    {
+        return Options.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Apps/Tisa.Store.Web.Ui/ViewComponents/TypeDropdownViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tisa.Store.Web.Ui.Data.Repositories.Contracts;
+using Tisa.Store.Web.Ui.Models.ViewModels.ViewComponents;
+
+namespace Tisa.Store.Web.Ui.ViewComponents;
+
+public class TypeDropdownViewComponent : ViewComponent
+{
+    public TypeDropdownViewComponent(ITypeRepository repository)
+    {
+        Repository = repository;
+    }
+
+    protected ITypeRepository Repository { get; }
+
+    public async Task<IViewComponentResult> InvokeAsync(int selected, string input)
+    {
+        return View(model: new TypeDropdownVm(
+            options: (await Repository.GetAsync(predicate: null))
+                .ToDictionary(
+                    keySelector: entity => entity.Id,
+                    elementSelector: entity => entity.Display
+                )
+            )
+        {
+            Selected = selected == 0 ? 1 : selected,
+            Input = input
+
+        });
+    }
+}
