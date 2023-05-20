0c59e45db4777aa33ce9ffd7adb9bfd69b1d9519
ahadvirus <ahadvirus@yahoo.com>
2023-05-20 15:36:16 +0330

    Extension to remove view component

---
+++ Apps/Tisa.Store.Web.Ui/Infrastructures/Extensions/RemoveViewComponentExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tisa.Store.Web.Ui.Infrastructures.Extensions;
+
+public static class RemoveViewComponentExtension
+{
+    public static string RemoveViewComponent(this string entry)
+    {
+        return entry.Replace(
+            oldValue: nameof(ViewComponent),
+            newValue: string.Empty
+        );
+    }
+}
