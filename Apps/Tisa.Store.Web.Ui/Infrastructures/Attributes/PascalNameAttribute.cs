8a05356ca3c87a090a48daa7ca1a48dcc5c2b37e
ahadvirus <ahadvirus@yahoo.com>
2023-06-01 16:55:28 +0330

    Change attribute type for localization

--- Apps/Tisa.Store.Web.Ui/Infrastructures/Attributes/PascalNameAttribute.cs
+++ Apps/Tisa.Store.Web.Ui/Infrastructures/Attributes/PascalNameAttribute.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 
 namespace Tisa.Store.Web.Ui.Infrastructures.Attributes;
@@ -6,7 +8,8 @@ namespace Tisa.Store.Web.Ui.Infrastructures.Attributes;
 /// <summary>
 /// Validation attribute for validate the entry is start with capital character
 /// </summary>
-public class PascalNameAttribute : ValidationAttribute
+[AttributeUsage(validOn: AttributeTargets.Property, AllowMultiple = false)]
+public class PascalNameAttribute : RegularExpressionAttribute
 {
 
     protected string Pattern
@@ -17,16 +20,19 @@ public class PascalNameAttribute : ValidationAttribute
         }
     }
 
-    public PascalNameAttribute() : base(errorMessage: "Please start {0} with capital character")
+    public PascalNameAttribute() : base(pattern: "^[A-Z]$")
     {
     }
 
     public override bool IsValid(object? value)
     {
-        return value != null ?
-            (value is string ?
-                new RegularExpressionAttribute(pattern: Pattern).IsValid(((string)value).First()) :
-                false)
-            : false;
+        value = value != null ? (value is string ? ((string)value).First() : string.Empty) : string.Empty;
+        return base.IsValid(value: value);
     }
+    /*
+    public override string FormatErrorMessage(string name)
+    {
+        return string.Format(provider: CultureInfo.CurrentCulture, format: ErrorMessageString, args: new object?[] { name });
+    }
+    */
 }
