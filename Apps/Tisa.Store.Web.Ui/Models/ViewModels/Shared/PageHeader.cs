4a40bdfeda07746aaf9d5a12be77dc45c982ea0c
ahadvirus <ahadvirus@yahoo.com>
2023-06-01 16:56:10 +0330

    Change properties of titles for working better with localization

---
+++ Apps/Tisa.Store.Web.Ui/Models/ViewModels/Shared/PageHeader.cs
@@ -0,0 +1,17 @@
+﻿namespace Tisa.Store.Web.Ui.Models.ViewModels.Shared;
+
+public record PageHeader
+{
+    public PageHeader()
+    {
+        Title = string.Empty;
+        CreateButtonTitle = string.Empty;
+        CreateButtonLink = string.Empty;
+        HaveCreateButton = false;
+    }
+
+    public object? Title { get; init; }
+    public bool HaveCreateButton { get; init; }
+    public object? CreateButtonTitle { get; init; }
+    public string CreateButtonLink { get; init;}
+}
